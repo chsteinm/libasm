e6e5e50f05b1e8fd7c7b75b4ea316fc616d7a325
Christophe Steinmetz <chrstein@z1r6p4.42lyon.fr>
2025-11-15 14:12:02 +0100

    clean

--- ft_strdup.s
+++ ft_strdup.s
@@ -6,20 +6,18 @@ section .text
     global  ft_strdup
 
 ft_strdup:
-        push    rdi
         test    rdi, rdi
         jz      .error
+        push    rdi
         call    ft_strlen
         inc     rax
         mov     rdi, rax
         call    malloc
+        pop     rsi
         test    rax, rax
         jz      .error
-        pop     rsi
         mov     rdi, rax
         call    ft_strcpy
         ret
     .error:
-        pop     rdi
-        xor     rax, rax
         ret
